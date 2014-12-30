c0a715ab84d3e6cc636b617d68ca76b69eca205d
zeus <ishan.h@outlook.com>
2014-12-30 12:58:06 -0800

    Remove s3 vars because it requires my visa

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -7,8 +7,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-   storage (Rails.env.production? ? :fog : :file)
-
+  # storage (Rails.env.production? ? :fog : :file)
+    storage :file
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
--- config/initializers/setup_paperclip.rb
+++ config/initializers/setup_paperclip.rb
@@ -1,12 +1,12 @@
-if Rails.env.production? || Rails.env.staging?
-  Paperclip::Attachment.default_options.merge!({
-    storage: :s3,
-    s3_credentials: {
-      access_key_id: ENV['s3_access_key_id'],
-      secret_access_key: ENV['s3_secret_key_id'],
-      bucket: 'bucket_name'
-    }
-  });
-end
+#if Rails.env.production? || Rails.env.staging?
+ # Paperclip::Attachment.default_options.merge!({
+  #  storage: :s3,
+   # s3_credentials: {
+    #  access_key_id: ENV['s3_access_key_id'],
+     # secret_access_key: ENV['s3_secret_key_id'],
+     # bucket: 'bucket_name'
+  #  }
+#  });
+# end
 
 
